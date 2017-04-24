58519a3a07c0c5c465258810194fe07130c8458a
fimanishi <fimanishi@gmail.com>
2017-04-23 20:35:22 -0500

    regex for phones and emails

--- rePhoneEmails.py
+++ rePhoneEmails.py
@@ -4,28 +4,27 @@ import re
 
 def getPhones(text):
     getPhones = re.compile(r'''
-                               \(?\b      # optional parenthesis
-                               \d{3}
-                               [)\- ]?    # location code numbers
-                               \ ?
-                               \d{3}    # first three numbers
-                               [\- ]?   # optional separator
-                               \d{4}\b    # last four numbers
+                               \(?\b        # optional parenthesis
+                               \d{3}        # location numbers
+                               [)\- ]?\ ?   # optional separators
+                               \d{3}        # first three numbers
+                               [\- ]?       # optional separator
+                               \d{4}\b      # last four numbers
                             ''', re.VERBOSE)
     phones = getPhones.findall(text)
     return phones
 
 def getEmails(text):
     getEmails = re.compile(r'''
-                            [\w\-.]+         # email name
-                            @           # separator
-                            \w+         # domain
-                            \.\w{2,}       # . and type
+                            \b[\w\-.$]+     # email name
+                            @               # separator
+                            \w+             # domain
+                            \.\w{2,}\b      # . and type
                             ''', re.VERBOSE)
     emails = getEmails.findall(text)
     return emails
 
 
 if __name__ == "__main__":
-    print(getPhones("33789-281-9071 fhhfjs (4018) 384-2883 hfjsdfhusdifhsl 3378427482"))
-    print(getEmails("fimanishi@gmail.com sbruble fima@getc.edu fjsjfkds@afjkds"))
+    print(getPhones("33789-281-9071 fhhfjs (418) 384-2883 hfjsdfhusdifhsl 3378427482"))
+    print(getEmails("fimani$hi@gmail.com sbruble fima@getc.edu fjsj.fk_ds@afjkds.co"))
